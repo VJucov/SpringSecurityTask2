6ff929dbaa47dbaec7bcfaa201977005d3aa2683
Valeria Jucov <valeria.jucov@endava.com>
2020-11-08 14:24:31 +0200

    Spring Security JDBC Auth

---
+++ src/main/java/com/luv2code/springsecurity/demo/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.luv2code.springsecurity.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username,password,enabled FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username,authority FROM authorities WHERE username = ?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE")
+                .antMatchers("/leaders/**").hasAnyRole("MANAGER")
+                .antMatchers("/systems/**").hasAnyRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and().logout().permitAll()
+                .and().exceptionHandling().accessDeniedPage("/access-denied");
+    }
+}
---
+++ src/main/java/com/luv2code/springsecurity/demo/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.luv2code.springsecurity.demo.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
